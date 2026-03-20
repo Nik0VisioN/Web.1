65fa779726b099082690f67821b7c8e093dbbd34
NikoVisioN <darkmer3y@gmail.com>
2026-03-20 23:00:12 +0100

    1.05 - added light and dark mode

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const name = 1;
+
 console.log(name); 
+
+const btn = document.getElementById('theme-btn');
+
+// On load — apply the saved theme
+if (localStorage.getItem('theme') === 'dark') 
+{
+    document.body.classList.add('dark');
+    btn.textContent = '☀️ Light Mode';
+}
+
+btn.addEventListener('click', function() 
+{
+    
+  document.body.classList.toggle('dark');
+  // Change button text based on the current theme
+    if (document.body.classList.contains('dark')) 
+        {
+        btn.textContent = '☀️ Light Mode';
+        localStorage.setItem('theme', 'dark');
+        } 
+    else 
+        {
+        btn.textContent = '🌙 Dark Mode';
+        localStorage.setItem('theme', 'light');
+        }
+});
+// added class, if it doesn't exist, and removes it if it does.
